77e3b0903ab5a4a775e155f8897dfd2d1a63d6f0
mobileexpert1 <Amit96737>
2026-07-06 19:10:15 +0530

    add_about_page

--- payments/core/urls.py
+++ payments/core/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("about/", views.stripe_about_page, name="stripe_about_page"),
     path("item-details/", views.item_detail, name="item_detail"),
     path("create-checkout-session/<uuid:id>/", views.create_checkout_session, name="create_checkout_session"),
     path("webhook/", views.stripe_webhook, name="stripe_webhook"),
