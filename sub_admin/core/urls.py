de65c398615cd43a4613df2356af1c499b53bf21
mobileexpert1 <Amit96737>
2026-07-15 18:46:24 +0530

    working_custom_admin_panel

--- sub_admin/core/urls.py
+++ sub_admin/core/urls.py
@@ -3,4 +3,22 @@ from . import views
 
 urlpatterns = [
     path('dashboard/', views.sub_admin_dashboard, name="sub_admin_dashboard"),
+
+    # User Management Section
+    path('user-management/', views.user_management, name="user_management"),
+    path('user-details/<str:id>/', views.user_detail, name="user_detail"),
+    # path('user-update/<str:id>/', views.update_user, name="update_user"),
+
+    # Crud Management Section
+    path('crud-management/', views.crud_management, name="crud_management"),
+    # path('student-details/<str:id>/', views.student_detail, name="student_detail"),
+
+    # Crud Management Section
+    path('bank-management/', views.bank_management, name="bank_management"),
+
+    # Crud Management Section
+    path('stripe-management/', views.stripe_management, name="stripe_management"),
+
+    path('paypal-management/', views.paypal_management, name="paypal_management"),
+    path('settings/', views.sub_admin_settings, name="sub_admin_settings"),
 ]
--- sub_admin/core/views.py
+++ sub_admin/core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from sub_admin.core.services import sub_admin_required
 from users.models.users import User
@@ -6,14 +6,187 @@ from crud.models.student import Student
 from bank.models.account import BankAccount
 from bank.models.bank import BankName
 from payments.models.product import Item
+from django.db.models import Q
+from django.core.paginator import Paginator
+
 
 @login_required(login_url='user_login')
 @sub_admin_required
 def sub_admin_dashboard(request):
-    total_dev_users = User.objects.all().count()
+    total_dev_users = User.objects.exclude(id=request.user.id).count()
     total_students = Student.objects.all().count()
     total_banks_account = BankAccount.objects.count()
     total_items = Item.objects.all().count()
     total_subscription = User.objects.filter(has_subscription=True).count()
     total_register_bank = BankName.objects.all().count()
     return render(request, 'sub_admin/dashboard.html', locals())
+
+
+@sub_admin_required
+def user_management(request):
+    query = request.GET.get('q')
+    users = User.objects.exclude(id=request.user.id).order_by('-id')
+
+    if query:
+        query = query.lower()
+
+        if query == "active":
+            users = users.filter(is_active=True)
+
+        elif query == "inactive":
+            users = users.filter(is_active=False)
+
+        else:
+
+            users = users.filter(
+                Q(email__icontains=query) |
+                Q(first_name__icontains=query) |
+                Q(last_name__icontains=query) |
+                Q(gender__icontains=query) |
+                Q(phone_number__icontains=query)
+            )
+
+    paginator = Paginator(users, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'sub_admin/user_management.html', {
+        'page_obj': page_obj,
+    })
+
+
+@sub_admin_required
+def user_detail(request, id):
+    selected_user = get_object_or_404(User, id=id)
+
+    return render(request, 'sub_admin/user_profile.html', {
+        'selected_user': selected_user
+    })
+
+
+# @sub_admin_required
+# def update_user(request, id):
+#     user = User.objects.get(id=id)
+#
+#     if request.method == "POST":
+#         user.first_name = request.POST.get('first_name')
+#         user.last_name = request.POST.get('last_name')
+#         user.email = request.POST.get('email')
+#         user.phone_number = request.POST.get('phone_number')
+#         user.gender = request.POST.get('gender')
+#         user.biograph = request.POST.get('biograph')
+#
+#         user.is_sub_admin = True if request.POST.get('is_sub_admin') == 'on' else False
+#
+#         user.save()
+#
+#     return redirect('update_user',id=user.id)
+
+
+@sub_admin_required
+def crud_management(request):
+    query = request.GET.get('q')
+    students = Student.objects.all().order_by('-id')
+
+    if query:
+        query = query.lower()
+
+        if query == "active":
+            users = students.filter(is_active=True)
+
+        elif query == "inactive":
+            users = students.filter(is_active=False)
+
+        else:
+
+            students = students.filter(
+                Q(email__icontains=query) |
+                Q(first_name__icontains=query) |
+                Q(last_name__icontains=query) |
+                Q(gender__icontains=query) |
+                Q(phone_number__icontains=query)
+            )
+
+    paginator = Paginator(students, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'sub_admin/crud_management.html', locals())
+
+
+
+@sub_admin_required
+def bank_management(request):
+    query = request.GET.get('q')
+    bank_record = BankAccount.objects.all().order_by('-id')
+
+
+    if query:
+        query = query.lower()
+
+        if query == "active":
+            users = bank_record.filter(is_active=True)
+
+        elif query == "inactive":
+            users = bank_record.filter(is_active=False)
+
+        else:
+
+            bank_record = bank_record.filter(
+                Q(account_number__icontains=query) |
+                Q(customer_name__icontains=query) |
+                Q(email__icontains=query) |
+                Q(gender__icontains=query) |
+                Q(bank__name__icontains=query) |
+                Q(aadhar_number__icontains=query)
+            )
+
+    paginator = Paginator(bank_record, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'sub_admin/bank_management.html', locals())
+
+
+@sub_admin_required
+def stripe_management(request):
+    query = request.GET.get('q')
+    items = Item.objects.all().order_by('-created_at')
+
+    if query:
+        query = query.lower()
+
+        if query == "active":
+            users = items.filter(is_active=True)
+
+        elif query == "inactive":
+            users = items.filter(is_active=False)
+
+        else:
+
+            items = items.filter(
+                Q(title__icontains=query) |
+                Q(brand__icontains=query) |
+                Q(description=query) |
+                Q(price__icontains=query) |
+                Q(colors__icontains=query) |
+                Q(size__icontains=query) |
+                Q(category__icontains=query) |
+                Q(is_sold__icontains=query)
+            )
+
+    paginator = Paginator(items, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'sub_admin/stripe_management.html', locals())
+
+
+@sub_admin_required
+def paypal_management(request):
+    return render(request, 'sub_admin/paypal_management.html', locals())
+
+
+@sub_admin_required
+def sub_admin_settings(request):
+    return render(request, 'sub_admin/settings.html', locals())
