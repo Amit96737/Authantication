f055511c0f570d382d57ad008ffc348a6c314799
mobileexpert1 <Amit96737>
2026-07-23 11:25:31 +0530

    update_payments_app_code

--- payments/models/product.py
+++ payments/models/product.py
@@ -1,6 +1,7 @@
 from django.db import models
 from common.models.common import CommonFields
 from django.core.validators import FileExtensionValidator
+from users.models.users import User
 
 class Item(CommonFields):
     title = models.CharField(max_length=250)
@@ -14,4 +15,11 @@ class Item(CommonFields):
     size = models.CharField(max_length=120)
     category = models.CharField(max_length=120)
     is_sold = models.BooleanField(default=False)
+
+class FavouriteItem(CommonFields):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, null=True, blank=True)
+
+    class Meta:
+        unique_together = ('user', 'item')
 
