1349e033214dc96a5df8344f206824157fecb73d
mobileexpert1 <Amit96737>
2026-07-23 18:07:10 +0530

    update_code

--- payments/models/product.py
+++ payments/models/product.py
@@ -2,11 +2,12 @@ from django.db import models
 from common.models.common import CommonFields
 from django.core.validators import FileExtensionValidator
 from users.models.users import User
+from tinymce.models import HTMLField
 
 class Item(CommonFields):
     title = models.CharField(max_length=250)
     brand = models.CharField(max_length=120)
-    description = models.CharField(max_length=350)
+    description = HTMLField(null=True, blank=True)
     image = models.ImageField(upload_to="item_image", null=True, blank=True,
                                     validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png', 'jfif'])
                                                 ])
@@ -15,6 +16,17 @@ class Item(CommonFields):
     size = models.CharField(max_length=120)
     category = models.CharField(max_length=120)
     is_sold = models.BooleanField(default=False)
+    mrp = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    quantity = models.IntegerField(default=0)
+
+    @property
+    def discount_percentage(self):
+        if self.mrp and self.price and self.mrp > self.price:
+            return int(((self.mrp-self.price) / self.mrp) * 100)
+        return 0
+
+    def __str__(self):
+        return self.title
 
 class FavouriteItem(CommonFields):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
@@ -23,3 +35,17 @@ class FavouriteItem(CommonFields):
     class Meta:
         unique_together = ('user', 'item')
 
+class ItemImage(CommonFields):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, null=True, blank=True, related_name='images')
+    image = models.ImageField(
+        upload_to="item_images",
+        validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png', 'jfif'])]
+    )
+
+    def __str__(self):
+        return self.item.title
+
+class Cart(CommonFields):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, null=True, blank=True)
+    quantity = models.IntegerField(default=1)
